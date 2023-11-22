4cea0c13a00863e935dbe7b9422b23b605e13dd6
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-11-22 22:13:04 +0100

    test(cookies): testing setting cookies on client

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -144,9 +144,7 @@ const sendAuthentication = async (res, user) => {
 
   res.cookie(`refreshToken_${user.id}`, refreshToken, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: 'none',
-    secure: true,
+    httpOnly: false,
   });
 
   res.send({
