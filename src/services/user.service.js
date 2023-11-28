3be944220626f1441430e35b9ccd9553f870440a
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-11-28 20:02:46 +0100

    fix: added condition inside googleCreateNewUser

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -167,10 +167,21 @@ const updateEmail = async (confirmationToken) => {
 };
 
 const googleCreateNewUser = async (email, name) => {
+  const user = await User.findOne({ email });
+  const hashedPassword = await bcrypt.hash('1234567', 10); 
+
+  if (user) {
+    return User.update({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  }
+
   return User.create({
     name,
     email,
-    password: await bcrypt.hash('1234567', 10),
+    password: hashedPassword,
   });
 };
 
